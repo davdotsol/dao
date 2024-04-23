f9a5823e2f456e3be891c64be9867f915a4c20a2
dvauquelin <david.vauquelin@planisware.com>
2024-04-23 15:49:52 +0200

    Finalized DAO contract with tests

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -11,6 +11,29 @@ contract DAO {
     Token public token;
     uint256 public quorum;
 
+    struct Proposal {
+        uint256 id;
+        string name;
+        uint256 amount;
+        address payable recipient;
+        uint256 votes;
+        bool finalized;
+    }
+
+    uint256 public proposalCount;
+    mapping(uint256 => Proposal) public proposals;
+    mapping(address => mapping(uint256 => bool)) public votes;
+
+    event Propose(
+        uint256 id,
+        uint256 amount,
+        address recipient,
+        address creator
+    );
+
+    event Vote(uint256 id, address investor);
+    event Finalize(uint256 id);
+
     constructor(Token _token, uint256 _quorum) {
         owner = msg.sender;
         token = _token;
@@ -19,4 +42,59 @@ contract DAO {
 
     // Allow contract to receive ether
     receive() external payable {}
+
+    modifier onlyInvestor() {
+        require(token.balanceOf(msg.sender) > 0, "must be token holder");
+        _;
+    }
+
+    function createProposal(
+        string memory _name,
+        uint256 _amount,
+        address payable _recipient
+    ) external onlyInvestor {
+        require(address(this).balance >= _amount);
+
+        proposalCount++;
+        // Create a Proposal
+        // Ex1. Send 100 ETH to Tom
+        // Ex2. Send 50 ETH to Jane for Website development
+        // Ex3. Send 1 ETH to Julie for Marketing
+        proposals[proposalCount] = Proposal(
+            proposalCount,
+            _name,
+            _amount,
+            _recipient,
+            0,
+            false
+        );
+
+        emit Propose(proposalCount, _amount, _recipient, msg.sender);
+    }
+
+    function vote(uint256 _proposalId) external onlyInvestor {
+        // Don't let investors vote twice
+        require(!votes[msg.sender][_proposalId], "Already voted");
+        Proposal storage proposal = proposals[_proposalId];
+
+        proposal.votes += token.balanceOf(msg.sender);
+
+        votes[msg.sender][_proposalId] = true;
+
+        emit Vote(_proposalId, msg.sender);
+    }
+
+    function finalizeProposal(uint256 _proposalId) external onlyInvestor {
+        Proposal storage proposal = proposals[_proposalId];
+        require(!proposal.finalized, "proposal already finalized");
+        require(
+            proposal.votes >= quorum,
+            "must reach quorum to finalize proposal"
+        );
+        require(address(this).balance >= proposal.amount);
+        proposal.finalized = true;
+        (bool sent, ) = proposal.recipient.call{value: proposal.amount}("");
+        require(sent);
+        emit Finalize(_proposalId);
+    }
 }
--- test/DAO.js
+++ test/DAO.js
@@ -10,13 +10,50 @@ const tokens = (m) => {
 
 describe('DAO contract', function () {
   async function deployDAOFixture() {
-    const [owner, funder, addr2] = await ethers.getSigners();
+    const [
+      owner,
+      funder,
+      investor1,
+      investor2,
+      investor3,
+      investor4,
+      investor5,
+      recipient,
+      user,
+    ] = await ethers.getSigners();
 
     const Token = await ethers.getContractFactory('Token');
     const ddsToken = await Token.deploy('Dapp Dot Sol', 'DDS', '1000000');
     await ddsToken.waitForDeployment();
 
+    // Send tokens to investors - each one gets 20%
+    let tx = await ddsToken
+      .connect(owner)
+      .transfer(investor1.address, tokens(200000));
+    await tx.wait();
+
+    tx = await ddsToken
+      .connect(owner)
+      .transfer(investor2.address, tokens(200000));
+    await tx.wait();
+
+    tx = await ddsToken
+      .connect(owner)
+      .transfer(investor3.address, tokens(200000));
+    await tx.wait();
+
+    tx = await ddsToken
+      .connect(owner)
+      .transfer(investor4.address, tokens(200000));
+    await tx.wait();
+
+    tx = await ddsToken
+      .connect(owner)
+      .transfer(investor5.address, tokens(200000));
+    await tx.wait();
+
     const DAO = await ethers.getContractFactory('DAO');
+    // Set Quorum to > 50% of the token total supply 500k tokens + 1wei, i.e., 500000000000000000000001
     const dao = await DAO.deploy(ddsToken.target, '500000000000000000000001');
     await dao.waitForDeployment();
 
@@ -25,7 +62,72 @@ describe('DAO contract', function () {
       value: ethers.parseEther('100'),
     });
 
-    return { dao, ddsToken, owner, funder, addr2 };
+    return {
+      dao,
+      ddsToken,
+      owner,
+      funder,
+      investor1,
+      investor2,
+      investor3,
+      investor4,
+      investor5,
+      recipient,
+      user,
+    };
+  }
+
+  async function createProposalFixture() {
+    const { dao, investor1, investor2, investor3, recipient, user } =
+      await loadFixture(deployDAOFixture);
+    const transaction = await dao
+      .connect(investor1)
+      .createProposal(
+        'Proposal 1',
+        ethers.parseEther('100'),
+        recipient.address
+      );
+
+    await transaction.wait();
+    return {
+      dao,
+      investor1,
+      investor2,
+      investor3,
+      recipient,
+      transaction,
+      user,
+    };
+  }
+
+  async function voteFixture() {
+    const { dao, investor1, investor2, investor3, recipient, user } =
+      await loadFixture(createProposalFixture);
+    const transaction = await dao.connect(investor1).vote(1);
+    await transaction.wait();
+
+    return {
+      dao,
+      investor1,
+      investor2,
+      investor3,
+      recipient,
+      transaction,
+      user,
+    };
+  }
+
+  async function governanceFixture() {
+    const { dao, investor1, investor2, investor3, recipient } =
+      await loadFixture(voteFixture);
+    let transaction = await dao.connect(investor2).vote(1);
+    await transaction.wait();
+    transaction = await dao.connect(investor3).vote(1);
+    await transaction.wait();
+    transaction = await dao.connect(investor1).finalizeProposal(1);
+    await transaction.wait();
+
+    return { dao, investor1, recipient, transaction };
   }
 
   describe('Deployment', function () {
@@ -43,6 +145,128 @@ describe('DAO contract', function () {
     it('should return the correct quorum', async function () {
       const { dao } = await loadFixture(deployDAOFixture);
       expect(await dao.quorum()).to.equal('500000000000000000000001');
+    });
+  });
+
+  describe('Create Proposals', function () {
+    it('should updates proposal count', async function () {
+      const { dao } = await loadFixture(createProposalFixture);
+      expect(await dao.proposalCount()).to.equal(1);
+    });
+
+    it('should updates proposal mapping', async function () {
+      const { dao, recipient } = await loadFixture(createProposalFixture);
+      const proposal = await dao.proposals(1);
+      expect(proposal.id).to.equal(1);
+      expect(proposal.amount).to.equal(ethers.parseEther('100'));
+      expect(proposal.recipient).to.equal(recipient.address);
+    });
+
+    it('should emit a Propose event', async function () {
+      const { dao, investor1, recipient, transaction } = await loadFixture(
+        createProposalFixture
+      );
+      await expect(transaction)
+        .to.emit(dao, 'Propose')
+        .withArgs(
+          1,
+          ethers.parseEther('100'),
+          recipient.address,
+          investor1.address
+        );
+    });
+
+    it('should reject invalid amount', async function () {
+      const { dao, investor1, recipient } = await loadFixture(deployDAOFixture);
+      await expect(
+        dao
+          .connect(investor1)
+          .createProposal(
+            'Proposal 1',
+            ethers.parseEther('1000'),
+            recipient.address
+          )
+      ).to.be.reverted;
+    });
+
+    it('should reject non investor', async function () {
+      const { dao, user, recipient } = await loadFixture(deployDAOFixture);
+      await expect(
+        dao
+          .connect(user)
+          .createProposal(
+            'Proposal 1',
+            ethers.parseEther('100'),
+            recipient.address
+          )
+      ).to.be.reverted;
+    });
+  });
+
+  describe('Voting', function () {
+    it('should update vote count', async function () {
+      const { dao } = await loadFixture(voteFixture);
+      const proposal = await dao.proposals(1);
+      expect(proposal.votes).to.equal(tokens(200000));
+    });
+
+    it('should reject non investor', async function () {
+      const { dao, user } = await loadFixture(createProposalFixture);
+      await expect(dao.connect(user).vote(1)).to.be.reverted;
+    });
+
+    it('should reject double voting', async function () {
+      const { dao, investor1 } = await loadFixture(voteFixture);
+      await expect(dao.connect(investor1).vote(1)).to.be.reverted;
+    });
+
+    it('should emit vote event', async function () {
+      const { dao, investor1, transaction } = await loadFixture(voteFixture);
+      await expect(transaction)
+        .to.emit(dao, 'Vote')
+        .withArgs(1, investor1.address);
+    });
+  });
+
+  describe('Governance', function () {
+    it('should update the proposal to finalized', async function () {
+      const { dao } = await loadFixture(governanceFixture);
+      const proposal = await dao.proposals(1);
+      expect(proposal.finalized).to.equal(true);
+    });
+
+    it('should transfer funds to recipient', async function () {
+      const { dao, recipient } = await loadFixture(governanceFixture);
+      expect(await ethers.provider.getBalance(recipient.address)).to.equals(
+        tokens(10100)
+      );
+    });
+
+    it('should emit vote event', async function () {
+      const { dao, transaction } = await loadFixture(governanceFixture);
+      await expect(transaction).to.emit(dao, 'Finalize').withArgs(1);
+    });
+
+    it('should reject proposal if already finalized', async function () {
+      const { dao, investor1 } = await loadFixture(governanceFixture);
+      await expect(dao.connect(investor1).finalizeProposal(1)).to.be.reverted;
+    });
+
+    it('should reject finalization if not enough votes', async function () {
+      const { dao, investor1 } = await loadFixture(voteFixture);
+      await expect(dao.connect(investor1).finalizeProposal(1)).to.be.reverted;
+    });
+
+    it('should reject finalization if not enough votes', async function () {
+      const { dao, investor1, investor2, investor3, user } = await loadFixture(
+        voteFixture
+      );
+      let tx = await dao.connect(investor2).vote(1);
+      await tx.wait();
+      tx = await dao.connect(investor3).vote(1);
+      await tx.wait();
+
+      await expect(dao.connect(user).finalizeProposal(1)).to.be.reverted;
     });
   });
 });
